977e7b082e559652ded49f5b9579d83eb394ca4d
Tim <vunvun0213@gmail.com>
2022-10-17 01:26:37 +0800

    加入專案檔案。

---
+++ Exec_DeclareSring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareSring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "C:\\temp\\a.jpg";
+            Console.WriteLine(sentence);
+            string senterce1 = "我是一個\"好學生\"" + ".";
+            Console.WriteLine(senterce1);
+            string senterce2 = "我是第三行文字\n我是第四行文字";
+            Console.WriteLine(senterce2);
+        }
+    }
+}
